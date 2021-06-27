4fa417261e9eef9896e558b00ab5fadaa8b598b6
Prince Virani <78205448+PBVirani@users.noreply.github.com>
2021-06-27 08:36:20 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>HTML - CSS </h1>
+      <p>Basic of Html and css.</p>
+    </div>
+  );
+}
+export default Info;
